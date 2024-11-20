02ca69ae5392e25f367730356e8a4b8d400f6be6
sergioizaiasparreiras <sergioparreiras09@gmail.com>
2024-11-20 19:29:12 -0300

    feat/implementando leitura em data hora

--- data_hora/instanciando_dataHora/src/App2.java
+++ data_hora/instanciando_dataHora/src/App2.java
@@ -1,4 +1,6 @@
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
@@ -6,12 +8,24 @@ import java.time.format.DateTimeFormatter;
 public class App2 {
     public static void main(String[] args) {
         
-        
+        LocalDate d04 = LocalDate.parse("2022-11-23");
+        LocalDateTime d05 = LocalDateTime.parse("2022-11-23T01:30:23");
         Instant d06 = Instant.parse("2024-07-23T01:30:23Z");
 
-        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        System.out.println("d06 = " + fmt3.format(d06));
+        //DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        
+        System.out.println("d04 = " + d04.format(fmt1)); // -> passando o formato como parametro
+
+        System.out.println("d04 = " + fmt1.format(d04)); // -> passando a hora instanciada como parametro
+
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); 
+        // -> passando a instanciação de dateTimeFormatter como parametro
+
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + fmt2.format(d05));
 
 
 
